f204b08074288da4e8cddeb1b967aae54b958b4c
Arabella Fanisheba <arabella.mutende@strathmore.edu>
2023-12-02 04:15:12 +0300

    Splash Screen implementation.

---
+++ app/src/main/java/com/example/fliccsy/SplashActivity.kt
@@ -0,0 +1,61 @@
+package com.example.fliccsy
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.fliccsy.ui.theme.FliccsyTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FliccsyTheme {
+                SplashScreen()
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    private fun SplashScreen() {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFF000000)),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(id = R.drawable.fliccsy_logo),
+                    contentDescription = null // descriptive text for screen readers
+                )
+//                Text(
+//                    text = "Fliccsy",
+//                    color = Color.Red,
+//                    fontSize = 20.sp, // Set the font size as per your design
+//                    modifier = Modifier.padding(top = 16.dp) // Add space between the image and text
+//                )
+            }
+        }
+    }
+}
